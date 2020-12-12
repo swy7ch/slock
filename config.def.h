7c322470fd2b9e3af4ea5620a163dbe9a1c37350
swy7ch <swy7ch@protonmail.com>
2020-12-12 21:04:37 +0000

    feat: ad custom colors and user settings

--- config.def.h
+++ config.def.h
@@ -1,11 +1,18 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "swytch";
+static const char *group = "swytch";
+static const char col_orange[]      = "#f7a583";
+static const char col_blue[]        = "#458588";
+static const char col_purple[]      = "#d3869b";
+static const char col_white[]       = "#ebdbb2";
+static const char col_black[]       = "#1d2021";
+static const char col_gray[]        = "#373737";
+static const char col_lightgray[]   = "#a89984";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   col_black,    /* after initialization */
+	[INPUT] =  col_blue,     /* during input */
+	[FAILED] = col_orange,   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
